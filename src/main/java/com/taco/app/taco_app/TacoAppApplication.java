e2e037988f81a3d26a576dca98562bcb3acbadc9
shivl <shivlall0009@gmail.com>
2024-12-16 18:42:17 -0500

    Completed initial project set up.

---
+++ src/main/java/com/taco/app/taco_app/TacoAppApplication.java
@@ -0,0 +1,16 @@
+package com.taco.app.taco_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TacoAppApplication.class, args);
+
+
+	}
+
+}
